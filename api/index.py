ca8d8b22f82c9dc3cb5d0b2310528a0265737590
KobraOp <professionalramiz9517@gmail.com>
2025-04-02 13:40:07 +0530

    change

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/developer')
+def dev():
+    return render_template('developer.html')
